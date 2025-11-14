52dbe84d92f82ab83438a9301e7f8e82d16c001e
TasteDeRambo <anonimous3746@gmail.com>
2025-11-14 16:00:31 -0600

    fix #3

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import wasm from "@rollup/wasm-node";
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      plugins: [wasm()]
-    }
-  }
+  plugins: [react()]
 });
